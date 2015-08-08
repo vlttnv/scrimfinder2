43a2b3b9b16119c4aca9267f525d098bff261195
Valentin Tunev <hdbuster@gmail.com>
2015-08-08 04:56:40 -0400

    removed db_ files

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from tornado.ioloop import IOLoop
 from tornado.wsgi import WSGIContainer
 from scrim2.extensions import db
 import sys, os.path
+import imp
 from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
@@ -23,7 +24,7 @@ if __name__ == "__main__":
             ], debug=True)
         server.listen(8080)
         IOLoop.instance().start()
-    elif sys.argv[1] == 'db':
+    elif sys.argv[1] == 'db_create':
         with scrim2_app.app_context():
             db.create_all()
         if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
@@ -31,4 +32,20 @@ if __name__ == "__main__":
             api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
         else:
              api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+    elif sys.argv[1] == 'db_migrate':
+        v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+        migration = SQLALCHEMY_MIGRATE_REPO + ('/versions/%03d_migration.py' % (v+1))
+        tmp_module = imp.new_module('old_model')
+        old_model = api.create_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+        exec(old_model, tmp_module.__dict__)
+        script = api.make_update_script_for_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, tmp_module.meta, db.metadata)
+        open(migration, "wt").write(script)
+        api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+        v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+        print('New migration saved as ' + migration)
+        print('Current database version: ' + str(v))
+    elif sys.argv[1] == 'db_upgrade':
+        api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+        v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+        print('Current database version: ' + str(v))
 
